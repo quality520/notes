b74c68519519da106aa49e5e6e88893f112d1606
quality520 <silencly520@163.com>
2016-06-27 00:35:59 +0800

    创建验证规则 compare指令

--- imooc/angularJs表单验证/js/signup.js
+++ imooc/angularJs表单验证/js/signup.js
@@ -12,3 +12,21 @@ angular.module('mainApp',[])
 			}
 		};
 	})
+	
+	.directive('compare',function(){
+		var o = {};
+		o.strict = 'AE';
+		o.scope = {
+			orgText : '=compare'
+		};
+		o.require = 'ngModel';
+		o.link = function(sco, ele, att, con){
+			con.$validators.compare = function(v){
+				return v == sco.orgText;
+			}
+			sco.$watch('orgText',function(){
+				con.$validate();
+			});
+		}
+		return o;
+	})
