74b70d7644e6a0741b13195768a58639675ef538
quality520 <silencly520@163.com>
2016-07-01 08:42:04 +0800

    倒计时效果

--- imooc/炫丽的倒计时效果Canvas绘图与动画基础/倒计时电子钟的实现/countdown.js
+++ imooc/炫丽的倒计时效果Canvas绘图与动画基础/倒计时电子钟的实现/countdown.js
@@ -14,7 +14,12 @@ window.onload = function(){
 	canvas.height = WINDOW_HEIGHT;
 	
 	curShowTimeSeconds = getCurrentShowTimeSeconds();
-	render(context);
+	
+	setInterval(function(){
+		render(context);
+		update();
+		console.log(new Date().valueOf());
+	},500)
 }
 function getCurrentShowTimeSeconds(){
 	var curTime = new Date();
@@ -22,10 +27,28 @@ function getCurrentShowTimeSeconds(){
 	ret = Math.round(ret / 1000);
 	return ret >= 0 ? ret : 0;
 }
-
+function update(){
+	
+	var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+	
+	var nextHours = parseInt(nextShowTimeSeconds / 3600);
+	var nextMinutes = parseInt((nextShowTimeSeconds - nextHours * 3600) / 60);
+	var nextSeconds = nextShowTimeSeconds % 60;
+	
+	var curHours = parseInt(curShowTimeSeconds / 3600);
+	var curMinutes = parseInt((curShowTimeSeconds - nextHours * 3600) / 60);
+	var curSeconds = curShowTimeSeconds % 60;
+	
+	if(nextSeconds != curSeconds){
+		curShowTimeSeconds = nextShowTimeSeconds;
+	}
+}
 
 
 function render(cxt){
+	
+	cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
 	var hours = parseInt(curShowTimeSeconds / 3600);
 	var minutes = parseInt((curShowTimeSeconds - hours*3600)/60)
 	var seconds = curShowTimeSeconds % 60;
